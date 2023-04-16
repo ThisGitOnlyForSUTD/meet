68a1041ff848b505d042f27b6881c0013de59656
Данила Романович <voloshenkoinc@artlebedev.ru>
2023-04-16 16:12:19 +0300

    skin api

--- stores/BannersStore.js
+++ stores/BannersStore.js
@@ -1,9 +1,23 @@
 import { defineStore } from 'pinia'
-import { ref, watch } from 'vue'
+import { ref } from 'vue'
 
 export const useBannersStore = defineStore('bannersStore', () => {
-  const { data } = useFetch(() => `/api/banners`)
-  const banners = ref(data)
+  const banners = ref([
+    {
+      id: '1',
+      title: 'Размер моей стопы',
+      img: 'vansBg',
+      size: 'EU: 42',
+      link: '/some-path'
+    },
+    {
+      id: '2',
+      title: 'Размер одежды',
+      img: 'adidasBg',
+      size: 'EU: M',
+      link: '/some-path'
+    },
+  ])
   return {
     banners
   }
